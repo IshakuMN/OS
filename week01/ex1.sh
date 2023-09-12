0b3286ca2ac2f7d9dc456e4fb02f09a2c3373370
IshakuMN <104638451+IshakuMN@users.noreply.github.com>
2023-09-12 12:49:42 +0300

    Update ex1.sh

--- week01/ex1.sh
+++ week01/ex1.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
 
 # Create the directory
 mkdir ~/week01
 
-# Navigate to the directory (though for this script's purposes, this step 
-isn't strictly necessary)
+# Navigate to the directory 
 cd ~/week01
 
 # Get the last 5 entries containing 'gcc' in reverse alphabetical order 
